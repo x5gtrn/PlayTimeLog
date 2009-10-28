4771777a9db16df28fa2b73da8f1fd696dde3c6b
x5gtrn <daisuke.masuda@gmail.com>
2009-10-28 23:59:37 +0900

    change saving method.

--- PTLSave.lua
+++ PTLSave.lua
@@ -7,23 +7,26 @@ function PlayTimeLog_SavePlayTime (startTime, playTime)
 		local addingTime = PlayTimeLog_GetTruncDay(t, 1) - t
 		PlayTimeLog_AddPlayTimeToDailyLog(PlayTimeLog_GetTruncDay(t), addingTime)
 		playTime = playTime - addingTime
-		t = PlayTimeLog_GetNextDay(t, 1)
+		t = PlayTimeLog_GetTruncDay(t, 1)
 	end
 	PlayTimeLog_AddPlayTimeToDailyLog(PlayTimeLog_GetTruncDay(t), playTime)
 	return PlayTimeLog_GetTruncDay(t) -- return last saved time as the key for debug.
 end
 
 function PlayTimeLog_AddPlayTimeToDailyLog (date, playTime)
+	local strDate = tostring(date)
 	-- add play time per account.
-	if (PlayTimeLogDB.dailyLog[date]) then
-		PlayTimeLogDB.dailyLog[date] = PlayTimeLogDB.dailyLog[date] + playTime
+	if (PlayTimeLogDB.dailyLog[strDate]) then
+		PlayTimeLogDB.dailyLog[strDate] = PlayTimeLogDB.dailyLog[strDate] + playTime
 	else
-		PlayTimeLogDB.dailyLog[date] = playTime
+		PlayTimeLogDB.dailyLog[#PlayTimeLogDB.dailyLog + 1] = date
+		PlayTimeLogDB.dailyLog[strDate] = playTime
 	end
 	-- add play time per character.
-	if (PlayTimeLogPCDB.dailyLog[date]) then
-		PlayTimeLogPCDB.dailyLog[date] = PlayTimeLogPCDB.dailyLog[date] + playTime
+	if (PlayTimeLogPCDB.dailyLog[strDate]) then
+		PlayTimeLogPCDB.dailyLog[strDate] = PlayTimeLogPCDB.dailyLog[strDate] + playTime
 	else
-		PlayTimeLogPCDB.dailyLog[date] = playTime
+		PlayTimeLogPCDB.dailyLog[#PlayTimeLogPCDB.dailyLog + 1] = date
+		PlayTimeLogPCDB.dailyLog[strDate] = playTime
 	end
 end
